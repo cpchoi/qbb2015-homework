92d32c9ffba599bd1885bae3e1037589d481f738
Charlotte Choi <cpchoi@jhu.edu>
2015-08-25 11:24:37 -0400

    filter lines 10-15

--- day2/filter.py
+++ day2/filter.py
@@ -5,8 +5,24 @@ filename = "/Users/cmdb/qbb2015-homework/day2/stringtie/SRR072893/t_data.ctab"
 #files are iterables - they can be used in a for loop
 f = open( filename )
 
+"""
+line_count = 0
 for data in f:
-    fields = data.split() # makes a list by splitting all the white space in each string (line
-    if "tRNA" in fields[9]: #column 10 - but python starts at 0. Count 10 columns in each string.
-        print data, #the comma, prevents printing a brand new line. also data is just a variable - it can be anything you want.
+    if line_count <= 10:
+        pass 
+    elif line_count <=15:
+        print data,
+    else:
+        break
+    line_count += 1
     
+    instead use enumerate:
+"""
+    
+for line_count, data in enumerate( f ):
+    if line_count <= 10:
+        pass 
+    elif line_count <=15:
+        print data,
+    else:
+        break
