e69f32798fd15ea67bde1ac84b8e73c72a631b04
Charlotte Choi <cpchoi@jhu.edu>
2015-08-26 09:28:54 -0400

    A new way to number 2

--- day2-hw/2.py
+++ day2-hw/2.py
@@ -10,13 +10,25 @@ df = pd.read_table( annotation, comment = '#', header = None )
 df.columns = [ "chromosome", "database", "type", "start", "end", "score", "strand", "frame", "attributes" ]
 
 
+#roi = df["attributes"].str.contains( "Sxl" )
+#other_roi = df["type"].str.contains( "transcript" )
+
+#print df[roi][other_roi] # this will give u a user warning - becuase the number of rows in roi and other_roi are different. 
+
+# anothe way:
+
 roi = df["attributes"].str.contains( "Sxl" )
-other_roi = df["type"].str.contains( "transcript" )
+df2 = df[roi]
+roi2 = df2["type"].str.contains("transcript")
+df3 = df2[roi2]
+print df3
+
 
 
 plt.figure()
 plt.title("plotted start positions")
-plt.plot(df[roi][other_roi]["start"])#this is saying plot the starts with the two sets of conditions set out in roi and other roi 
+plt.plot(df2[roi2]["start"])
+#plt.plot(df[roi][other_roi]["start"])#this is saying plot the starts with the two sets of conditions set out in roi and other roi 
 plt.xlabel("gene")
 plt.ylabel("sxl start position that are transcripts")
 plt.savefig("start_sxl_transcript.png") # in terminal you can open all the files by open *.png
